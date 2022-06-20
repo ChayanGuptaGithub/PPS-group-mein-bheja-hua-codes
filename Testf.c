2bd3f0e016b5412896a96147580674b15d19f337
ChayanGuptaGithub <93777077+ChayanGuptaGithub@users.noreply.github.com>
2022-06-20 20:31:27 +0530

    Create Testf.c

---
+++ Testf.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c,d,e,f;
+FILE *fptr1,*fptr2;
+fptr1=fopen("text.txt","r");
+fscanf(fptr1,"%d %d %d",&a,&b,&c);
+d=a+b;
+e=b+c;
+f=c+a;
+fclose(fptr1);
+fptr2=fopen("result.txt","w");
+fprintf(fptr2,"Result=%d %d %d",d,e,f);
+fclose(fptr2);
+}
